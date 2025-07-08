ed8ed23b485f53bfe120674871000027eb663aac
Medipally-vinay <vinay.medipally@winfosolutions.com>
2025-07-08 15:32:20 +0530

    deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import {FormComponentComponent} from "./form-component/form-component.component";
 import { BudgetresultComponent } from './budgetresult/budgetresult.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+// import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
-    { path : '',component : LoginPageComponent},
-    { path : 'form',component : FormComponentComponent},
+    // { path : '',component : LoginPageComponent},
+    { path : '',component : FormComponentComponent},
     {path:'budgetresult',component:BudgetresultComponent}
 ];
